7e2bdfb11e9a4f98d207f5cf876c49573b46c333
sandeep <sandeepdoddamani7@gmail.com>
2025-03-30 15:17:25 +0530

    infra directory addedd with configuration files

---
+++ infra/setup/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "devops-automatio-bucket"
+    key = "tf-state-setup"
+    region = "ap-south-1"
+    encrypt = true
+    dynamodb_table = "devops-automatio-dynomodb-table"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project = var.Project
+      contact = var.contact
+      ManageBy = "Terraform/setup"
+    }
+  }
+}
---
+++ infra/setup/variables.tf
@@ -0,0 +1,20 @@
+variable "tf_state_bucket" {
+  description = "name of s3 bucket for storing TF state"
+  default = "devops-automatio-bucket"
+}
+
+variable "tf_state_lock_table" {
+  description = "name of the DynomoDB table for TF state locking"
+  default = "devops-automatio-dynomodb-table"
+}
+
+variable "Project" {
+  description = "Project name for tagging resources"
+  default = "recepi-app-api"
+}
+
+variable "contact" {
+  description = "contact name fr tagging"
+  default = "sandeepdoddamani7@gmail.com"
+}
+
