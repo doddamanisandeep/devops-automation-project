5b36cb5a1f045fd0015aa0a52d7b23e9bdd73033
doddamanisandeep <sandeepdoddamani7@gmail.com>
2025-03-30 18:39:32 +0530

    fmt and validate

--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -1,20 +1,20 @@
 variable "tf_state_bucket" {
   description = "name of s3 bucket for storing TF state"
-  default = "devops-automation-bkt"
+  default     = "devops-automation-bkt"
 }
 
 variable "tf_state_lock_table" {
   description = "name of the DynomoDB table for TF state locking"
-  default = "devops-automatio-dynomodb-table"
+  default     = "devops-automatio-dynomodb-table"
 }
 
 variable "project" {
   description = "Project name for tagging resources"
-  default = "recepi-app-api"
+  default     = "recepi-app-api"
 }
 
 variable "contact" {
   description = "contact name fr tagging"
-  default = "sandeepdoddamani7@gmail.com"
+  default     = "sandeepdoddamani7@gmail.com"
 }
 
