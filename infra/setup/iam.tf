f323fe287a030d6618301d511c51c572500fa1ae
doddamanisandeep <sandeepdoddamani7@gmail.com>
2025-03-31 19:44:21 +0530

    iam.tf modifed after errors-2

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -52,28 +52,40 @@ resource "aws_iam_user_policy_attachment" "tf_backend" {
   policy_arn = aws_iam_policy.tf_backend.arn
 }
 
-##############################################
-# Create ECR repos for storing Docker images #
-##############################################
+#########################
+# Policy for ECR access #
+#########################
 
-resource "aws_ecr_repository" "app" {
-  name                 = "recipe-app-api-app"
-  image_tag_mutability = "MUTABLE"
-  force_delete         = true
+data "aws_iam_policy_document" "ecr" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
 
-  image_scanning_configuration {
-    # NOTE: Update to true for real deployments.
-    scan_on_push = false
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage"
+    ]
+    resources = [
+      aws_ecr_repository.app.arn,
+      aws_ecr_repository.proxy.arn,
+    ]
   }
 }
 
-resource "aws_ecr_repository" "proxy" {
-  name                 = "recipe-app-api-proxy"
-  image_tag_mutability = "MUTABLE"
-  force_delete         = true
+resource "aws_iam_policy" "ecr" {
+  name        = "${aws_iam_user.cd.name}-ecr"
+  description = "Allow user to manage ECR resources"
+  policy      = data.aws_iam_policy_document.ecr.json
+}
 
-  image_scanning_configuration {
-    # NOTE: Update to true for real deployments.
-    scan_on_push = false
-  }
+resource "aws_iam_user_policy_attachment" "ecr" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.ecr.arn
 }
