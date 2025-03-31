f69ec4b504443eab19f7dc5728fc15c306c7672f
doddamanisandeep <sandeepdoddamani7@gmail.com>
2025-03-31 19:28:54 +0530

    iam.tf modifed after errors

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -20,43 +20,7 @@ data "aws_iam_policy_document" "tf_backend" {
     actions   = ["s3:ListBucket"]
     resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
   }
-  #########################
-  # Policy for ECR access #
-  #########################
 
-  data "aws_iam_policy_document" "ecr" {
-    statement {
-      effect    = "Allow"
-      actions   = ["ecr:GetAuthorizationToken"]
-      resources = ["*"]
-    }
-
-    statement {
-      effect = "Allow"
-      actions = [
-        "ecr:CompleteLayerUpload",
-        "ecr:UploadLayerPart",
-        "ecr:InitiateLayerUpload",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:PutImage"
-      ]
-      resources = [
-        aws_ecr_repository.app.arn,
-        aws_ecr_repository.proxy.arn,
-      ]
-    }
-  }
-
-  resource "aws_iam_policy" "ecr" {
-    name        = "${aws_iam_user.cd.name}-ecr"
-    description = "Allow user to manage ECR resources"
-    policy      = data.aws_iam_policy_document.ecr.json
-  }
-
-  resource "aws_iam_user_policy_attachment" "ecr" {
-    user       = aws_iam_user.cd.name
-    policy_arn = aws_iam_policy.ecr.arn
-  }
   statement {
     effect  = "Allow"
     actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
@@ -86,4 +50,30 @@ resource "aws_iam_policy" "tf_backend" {
 resource "aws_iam_user_policy_attachment" "tf_backend" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.tf_backend.arn
+}
+
+##############################################
+# Create ECR repos for storing Docker images #
+##############################################
+
+resource "aws_ecr_repository" "app" {
+  name                 = "recipe-app-api-app"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    # NOTE: Update to true for real deployments.
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository" "proxy" {
+  name                 = "recipe-app-api-proxy"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    # NOTE: Update to true for real deployments.
+    scan_on_push = false
+  }
 }
